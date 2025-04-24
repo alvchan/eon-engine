91c876fca833605ee0fb270de41e9858634b5279
Alvin Chan <altc.alchan@gmail.com>
2025-04-24 18:50:53 -0400

    Refactor into init-update model
    Done to decouple main and intends to mimic how the user API exposes game
    loop updates to streamline its implementation later.
    
    adds:
    - decoupled init-update model
    - fixed frame rate cycles (for later physics loop)

--- eon.c
+++ eon.c
@@ -1,6 +1,5 @@
 #include "SDL.h"
 
-#include <stdbool.h>
 #include <stdio.h>
 
 /* wrapper macros over common SDL calls */
@@ -10,13 +9,17 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+const unsigned int ffps = 60; /* fixed frame rate for physics */
+const Uint64 fft = 1000/ffps; /* ms/frame for 60 fps */
+
 SDL_Window *window = NULL;
 SDL_Surface *surf = NULL;
+SDL_Event event;
 
-static bool init(void) {
+static void init(void) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("[error] Couldn't initialize SDL. SDL_Error: %s\n", SDL_GetError());
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   }
 
   window = SDL_CreateWindow("SDL Tutorial",
@@ -25,35 +28,49 @@ static bool init(void) {
 
   if (window == NULL) {
     printf("[error] Couldn't create a window. SDL_Error: %s\n", SDL_GetError());
-    return EXIT_FAILURE;
+    exit(EXIT_FAILURE);
   }
 
   surf = SDL_GetWindowSurface(window);
   fillbg();
   SDL_UpdateWindowSurface(window);
-
-  return EXIT_SUCCESS;
 }
 
-int main(int argc, char **argv) {
-  /* can't recover from a startup panic, just quit */
-  if (init() == EXIT_FAILURE)
-    exit(EXIT_FAILURE);
+static void update(int i) {
+  Uint64 starttime = SDL_GetTicks();
 
   /* poll for quit event, hold window open until then */
-  SDL_Event e;
-  bool quit = false;
-  while (quit == false) {
-    while (SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT) {
-        quit = true;
-      }
+  while (SDL_PollEvent(&event)) {
+    if (event.type == SDL_QUIT) {
+      SDL_DestroyWindow(window);
+      SDL_Quit();
+      exit(EXIT_SUCCESS);
     }
   }
 
-  /* cleanup */
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  /* Game Loop */
+  printf("%d\n", i);
+
+  Uint64 endtime = SDL_GetTicks();
+  Uint64 elapsed = endtime - starttime;
+
+  /* delay frame to keep framerate ~constant */
+  Uint64 delay;
+  if (elapsed <= fft)
+    delay = fft - elapsed;
+  else
+    delay = 0;
+
+  SDL_Delay(delay);
+}
+
+int main(int argc, char **argv) {
+  init();
+
+  int i = 0;
+  while (1) {
+    update(i++);
+  }
 
   return EXIT_SUCCESS;
 }
